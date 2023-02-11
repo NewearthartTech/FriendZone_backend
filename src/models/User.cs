c15611bb2938a0fbc5fb5bab3068f643c4a2cc90
unknown <divine@newearthart.tech>
2023-02-11 19:42:23 +0100

    added backend code

---
+++ src/models/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace src.models
+{
+    [BsonIgnoreExtraElements]
+    [MongoCollection("users")]
+    public class User
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; } = "";
+        public string walletAddress { get; set; } = "";
+    }
+}
